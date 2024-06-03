5aba2355eec1fda8ea43e7cd1e84819acef6a61a
Olaf Kucharski <115740193+olaf-kucharski@users.noreply.github.com>
2024-06-03 21:16:44 +0200

    Changed Gaussian arguments to random

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import random
 
 images = ["landscape.png"]
 
@@ -8,13 +8,12 @@ image = cv2.imread("landscape.png")
 for img in images:
     print(img[0:img.index('.')], img[img.index('.'):])
     image = cv2.imread(f'{img}')
-    # cv2.imshow('ORIGINAL', image)
+    # losowanie rozmycia
+    rand = random.randrange(3, 16, 1)
+    print(rand)
+
     # definicja filtrów
-    Gaussian_7 = cv2.GaussianBlur(image, (7, 7), 0)
-    Gaussian_15 = cv2.GaussianBlur(image, (15, 15), 0)
-    # cv2.imshow('SHARPEN', Gaussian_7)
-    # cv2.waitKey(0)
+    Gaussian = cv2.GaussianBlur(image, (rand, rand), 0)
+
     # zapisywanie
-    cv2.imwrite(f"{img[0:img.index('.')]}_Gaussian7{img[img.index('.'):]}", Gaussian_7)
-    cv2.imwrite(f"{img[0:img.index('.')]}_Gaussian15{img[img.index('.'):]}", Gaussian_15)
-    # cv2.destroyAllWindows()
+    cv2.imwrite(f"{img[0:img.index('.')]}_Gaussian{rand}{img[img.index('.'):]}", Gaussian)
