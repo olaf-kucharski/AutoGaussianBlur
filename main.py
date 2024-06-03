4b07da8df5b4f28d8d613908621a46620c2f3008
Olaf Kucharski <115740193+olaf-kucharski@users.noreply.github.com>
2024-06-03 21:57:06 +0200

    Deleted unnecessary comments

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import random
 
 images = ["landscape.png", "doggos.jpeg"]
 
-image = cv2.imread("landscape.png")
-
 for img in images:
     print(img[0:img.index('.')], img[img.index('.'):])
     image = cv2.imread(f'C://Users/olafk/PycharmProjects/AutoGaussianBlur/{img}')
-    # print(image.shape)
 
     # losowanie rozmycia
-    rand = random.randrange(3, 21, 2)
+    rand = random.randrange(3, 16, 2)
     print(rand)
 
     # definicja filtrów
