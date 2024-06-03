81c747e46afce2268683478f738024116f977015
Olaf Kucharski <115740193+olaf-kucharski@users.noreply.github.com>
2024-06-03 22:14:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import cv2
 import random
+import os
 
 images = ["landscape.png", "doggos.jpeg"]
 
 for img in images:
     print(img[0:img.index('.')], img[img.index('.'):])
-    image = cv2.imread(f'C://Users/olafk/PycharmProjects/AutoGaussianBlur/{img}')
+    image = cv2.imread(f'Pictures/{img}')
 
     # losowanie rozmycia
     rand = random.randrange(3, 16, 2)
@@ -14,5 +15,7 @@ for img in images:
     # definicja filtrów
     Gaussian = cv2.GaussianBlur(image, (rand, rand), 0)
 
+    # folder docelowy
+    path = f'Pictures/'
     # zapisywanie
-    cv2.imwrite(f"{img[0:img.index('.')]}_Gaussian{img[img.index('.'):]}", Gaussian)
+    cv2.imwrite(f"{path}{img[0:img.index('.')]}_Gaussian{img[img.index('.'):]}", Gaussian)
