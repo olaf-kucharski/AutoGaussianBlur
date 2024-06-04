a5b0a8059a555aa3c768940233a3cc1cf962e187
Olaf Kucharski <115740193+olaf-kucharski@users.noreply.github.com>
2024-06-04 15:11:03 +0200

    Dodane zmiany miłosza

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import cv2
 import random
+import os
 
-# lista zdjęć
-images = ["landscape.png", "doggos.jpeg"]
+for img in os.listdir('Pictures'):
+    with open(os.path.join('Pictures', img)) as f:
+        print(f"{img[0:img.index('.')]}{img[img.index('.'):]}")
+        image = cv2.imread(f'Pictures/{img}')
 
-for img in images:
-    print(img[0:img.index('.')], img[img.index('.'):])
-    image = cv2.imread(f'Pictures/{img}')
+        # losowanie rozmycia
+        rand = random.randrange(3, 16, 2)
+        print(rand)
 
-    # losowanie rozmycia
-    rand = random.randrange(3, 16, 2)
-    print(rand)
+        # definicja filtrów
+        Gaussian = cv2.GaussianBlur(image, (rand, rand), 0)
 
-    # definicja filtrów
-    Gaussian = cv2.GaussianBlur(image, (rand, rand), 0)
+        # folder docelowy
+        path = f'Zgaussowane/'
 
-    # folder docelowy
-    path = f'Pictures/'
-
-    # zapisywanie
-    cv2.imwrite(f"{path}{img[0:img.index('.')]}_Gaussian{img[img.index('.'):]}", Gaussian)
+        # zapisywanie
+        cv2.imwrite(f"{path}{img[0:img.index('.')]}_Gaussian{img[img.index('.'):]}", Gaussian)
