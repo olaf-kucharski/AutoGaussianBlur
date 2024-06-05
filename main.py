75414ef3a43b2866e94aa558af10e615cfbc9e85
MajloszOK <okarmamilosz@gmail.com>
2024-06-05 15:44:31 +0200

    Obcinanie do 256

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ for img in os.listdir('Pictures'):
         print(f"{img[0:img.index('.')]}{img[img.index('.'):]}")
         image = cv2.imread(f'Pictures/{img}')
 
+        height, width, _ = image.shape
+        x_start = (width - 256) // 2
+        y_start = (height - 256) // 2
+        cropped_image = image[y_start:y_start + 256, x_start:x_start + 256]
+
+        cv2.imwrite(f'256X/{img}', cropped_image)
+
         # losowanie rozmycia
         rand = random.randrange(3, 16, 2)
         print(rand)
 
         # definicja filtrów
-        Gaussian = cv2.GaussianBlur(image, (rand, rand), 0)
+        Gaussian = cv2.GaussianBlur(cropped_image, (rand, rand), 0)
 
         # folder docelowy
         path = f'Zgaussowane/'
